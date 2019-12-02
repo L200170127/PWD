b7ee049a89c9e85f50ae9dedcd815217a9275cc0
L200170127 <43404039+L200170127@users.noreply.github.com>
2019-12-02 16:33:42 +0700

    Add files via upload

---
+++ MODUL_9/admin.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+    <title>Selamat Datang Admin</title>
+    <link rel="stylesheet" href="style.css" type="text/css">
+</head>
+<body>
+    <br>
+    <center><h1>Welcome Admin</h1>
+    Silahkan logout dengan klik link <a href="tugas.php" class="link">Disini</a></center>
+</body>
+</html>
